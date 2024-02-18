70a42c5e2cbe88fd42c6ecd7186a69fde664ccbb
abrish-seng <abrish814@gmail.com>
2024-02-18 03:32:47 -0800

    sorting lists

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - function that sorts a doubly linked lists
+ * of integers in ascending order using the Insertion sort algorithms
+ * @list: Dobule linked lists to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *node;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+	node = (*list)->next;
+	while (node)
+	{
+		while ((node->prev) && (node->prev->n > node->n))
+		{
+			node = swap_node(node, list);
+			print_list(*list);
+		}
+		node = node->next;
+	}
+}
+/**
+ *swap_node - swap a node for his previous values
+ *@node: node
+ *@list: node lists
+ *Return: return a pointer to a node which was entered
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
